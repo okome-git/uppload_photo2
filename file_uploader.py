3b076d38cce322597cbb978b21b107b9f30bf8f1
okome <67691153+okome-git@users.noreply.github.com>
2021-08-17 09:33:38 +0900

    Update file_uploader.py

--- file_uploader.py
+++ file_uploader.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 pre_password = st.secrets["PRE_PASSWORD"]
-input_password = st.text_input("パスワード", help="事前に事務局より通知された12桁以内のパスワードを入力してください", value="", type="password")
+input_password = st.text_input("パスワード", help="事前に事務局より通知されたパスワードを半角英数字で入力してください", value="", type="password")
 
 if str(pre_password) != str(input_password):
     st.warning('写真を投稿するにはパスワードを入力してください')
